28dc2acc19c886f194e747288527c12b1d2b541d
HarryChen0506 <chenwenhua0506@sina.com>
2019-08-03 16:59:08 +0800

    feat(test commit): test commit

--- src/typescript-demo.ts
+++ src/typescript-demo.ts
@@ -1,8 +1,10 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
+
 export default class DummyClass {
   say(str: string): void {
     console.log(`hello, ${str}`)
+    console.log('hello')
   }
 }
